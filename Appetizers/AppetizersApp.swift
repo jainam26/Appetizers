287cb4b4bbe00bce7e67b1a3ca62bba9401d54d9
Jainam Shah <jainam_s@icloud.com>
2021-01-08 12:11:26 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Jainam  Shah  on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
